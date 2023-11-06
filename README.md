8074f21c7c6882710efba0e3e0ce42b98bf49984
Dwiky Ahmad Megananta <dwikyahmad@gmail.com>
2023-11-06 21:04:27 +0700

    Done Tugas 7 + Bonus

--- README.md
+++ README.md
@@ -1,16 +1,24 @@
 # inventori
 
-A new Flutter project.
+## Tugas 7
 
-## Getting Started
+### 1. Apa perbedaan utama antara stateless dan stateful widget dalam konteks pengembangan aplikasi Flutter?
+Stateless widgets tidak mengubah tampilan UI setelah mereka dibuat, apabila data atau keadaan aplikasi berubah, maka diperlukan pembuatan ulang widget stateless dengan data yang diperbarui, sedangkan Stateful widgets memiliki state internal yang memungkinkan widget untuk merespons terhadap perubahan data dan keadaan dalam aplikasi.
 
-This project is a starting point for a Flutter application.
+### 2. Sebutkan seluruh widget yang kamu gunakan untuk menyelesaikan tugas ini dan jelaskan fungsinya masing-masing.
+1) MaterialApp: root widget dari aplikasi Flutter yang berfungsi menyediakan elemen-elemen dasar seperti tema dan title
+2) Scaffold: Widget yang menyediakan kerangka kerja dasar untuk halaman dengan AppBar, FloatingActionButton
+3) AppBar: Menampilkan bar di bagian atas aplikasi dengan judul yang ditentukan
+4) SingleChildScrollView: Membungkus child widget dengan tampilan scroll vertikal
+5) Padding: Digunakan untuk menambahkan padding pada child widget
+6) Column: Widget ini digunakan untuk mengatur child widget secara vertikal dalam satu kolom
+7) Text: Menampilkan teks pada tampilan
+8) GridView.count: Digunakan untuk membuat tata letak grid dengan jumlah kolom yang ditentukan
+9) Material: Ini adalah wrapper untuk merender kontennya dengan efek material design
+10) InkWell: Ini digunakan untuk membuat area yang responsif terhadap sentuhan pengguna
+11) Container: Digunakan untuk menyusun tampilan
+12) Icon: Menampilkan ikon
+13) SnackBar: Digunakan untuk menampilkan pesan cepat yang muncul sementara di bagian bawah layar setelah pengguna melakukan tindakan tertentu
 
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+### 3. Jelaskan bagaimana cara kamu mengimplementasikan checklist di atas secara step-by-step (bukan hanya sekadar mengikuti tutorial)
+Pertama-tama saya membuat folder proyek flutter dengan nama inventori dengan command `flutter create inventori`, lalu saya membuat file baru pada folder lib dengan nama `menu.dart`, dari main.dart class MyHomePage akan dipindahkan ke menu.dart dan class _MyHomePageState akan dihapus, lalu dari file `main.dart` akan diimport menu.dart untuk menggunakan class MyHomePage `import 'package:inventori/menu.dart';`, lalu pada main.dart home akan diubah kodenya menjadi MyHomePage() dan mengubah colorScheme menjadi warna Indigo, lalu pada menu.dart class MyHomePage akan diubah menjadi stateless widget dan merubah parameter menjadi `({Key? key}) : super(key: key);` lalu menambahkan widget Build yang mengembalikan Scaffold, setelah itu akan ditambahkan tombol dengan membuat class ShopItem dan ShopCard, setelah itu widget build akan diisi menggunakan widget untuk tampilan seperti SingleChildScrollView, Column, Padding, dll. Setelah itu ditambahkan widget ShopCard yang menerima parameter dari widget ShopItem yang telah didefinisikan pada list items, pada bonus class ShopCard diberi tambahan atribut color untuk memberi custom color pada ShopCard. 
---
+++ lib/menu.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final List<ShopItem> items = [
+    ShopItem("Lihat Item", Icons.checklist),
+    ShopItem("Tambah Item", Icons.add_shopping_cart),
+    ShopItem("Logout", Icons.logout),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Shopping List',
+        ),
+      ),
+      body: SingleChildScrollView(
+        // Widget wrapper yang dapat discroll
+        child: Padding(
+          padding: const EdgeInsets.all(10.0), // Set padding dari halaman
+          child: Column(
+            // Widget untuk menampilkan children secara vertikal
+            children: <Widget>[
+              const Padding(
+                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                // Widget Text untuk menampilkan tulisan dengan alignment center dan style yang sesuai
+                child: Text(
+                  'PBP Shop', // Text yang menandakan toko
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              // Grid layout
+              GridView.count(
+                // Container pada card kita.
+                primary: true,
+                padding: const EdgeInsets.all(20),
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                crossAxisCount: 3,
+                shrinkWrap: true,
+                // Membuat children ShopCard dengan custom color
+                children: [
+                  ShopCard(items[0], Colors.indigo, key: UniqueKey()),
+                  ShopCard(items[1], Colors.teal, key: UniqueKey()),
+                  ShopCard(items[2], Colors.deepOrange, key: UniqueKey()),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ShopItem {
+  final String name;
+  final IconData icon;
+
+  ShopItem(this.name, this.icon);
+}
+
+class ShopCard extends StatelessWidget {
+  final ShopItem item;
+
+  // Menambah atribut color
+  final Color color;
+
+  const ShopCard(this.item, this.color, {super.key}); // Constructor
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: color,
+      child: InkWell(
+        // Area responsive terhadap sentuhan
+        onTap: () {
+          // Memunculkan SnackBar ketika diklik
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(SnackBar(
+                content: Text("Kamu telah menekan tombol ${item.name}!")));
+        },
+        child: Container(
+          // Container untuk menyimpan Icon dan Text
+          padding: const EdgeInsets.all(8),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  item.icon,
+                  color: Colors.white,
+                  size: 30.0,
+                ),
+                const Padding(padding: EdgeInsets.all(3)),
+                Text(
+                  item.name,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
